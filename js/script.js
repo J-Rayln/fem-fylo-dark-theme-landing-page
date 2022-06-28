66cc712d9844065f9d0587dd60de5f66609fcea5
J-Rayln <103427109+J-Rayln@users.noreply.github.com>
2022-06-27 22:39:43 -0700

    added email verification

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function validation()
+{
+  var form = document.getElementById("email-signup");
+  var email = document.getElementById("signup__email").value;
+  var text = document.getElementById("error-text");
+var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  if (email.match(pattern))
+    {
+      form.classList.add("valid");
+      form.classList.remove("invalid");
+      text.innerHTML = "";
+    }
+  else
+  {
+    form.classList.remove("valid");
+    form.classList.add("invalid");
+    text.innerHTML = "Please enter a valid email address";
+  }
+   if (email == "")
+    {
+      form.classList.remove("valid");
+      form.classList.remove("invalid");
+      text.innerHTML = "";
+    }
+}
