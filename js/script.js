147eaad998db250802e7c6707dad0f8f2eb526e3
J-Rayln <103427109+J-Rayln@users.noreply.github.com>
2022-07-18 22:41:05 -0700

    fixed typo

--- js/script.js
+++ js/script.js
@@ -1,26 +1,21 @@
-function validation()
-{
+function validation() {
   var form = document.getElementById("email-signup");
   var email = document.getElementById("signup__email").value;
-  var text = document.getElementById("error-text");
-var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+  var text = document.getElementByID("error-text");
+  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
-  if (email.match(pattern))
-    {
-      form.classList.add("valid");
-      form.classList.remove("invalid");
-      text.innerHTML = "";
-    }
-  else
-  {
+  if (email.match(pattern)) {
+    form.classList.add("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "";
+  } else {
     form.classList.remove("valid");
     form.classList.add("invalid");
     text.innerHTML = "Please enter a valid email address";
   }
-   if (email == "")
-    {
-      form.classList.remove("valid");
-      form.classList.remove("invalid");
-      text.innerHTML = "";
-    }
+  if (email == "") {
+    form.classList.remove("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "";
+  }
 }
